a1d918be1a43ffb5c6076ce6b20882e8cb1af171
ESKEN <msremacki@yahoo.com>
2019-05-16 22:21:38 +0200

    adding files

---
+++ js/main.js
@@ -0,0 +1,101 @@
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+function myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+}
+function myFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+}
+function myFunction4() {
+    var popup = document.getElementById("myPopup4");
+    popup.classList.toggle("show");
+}
+function myFunction5() {
+    var popup = document.getElementById("myPopup5");
+    popup.classList.toggle("show");
+}
+function myFunction6() {
+    var popup = document.getElementById("myPopup6");
+    popup.classList.toggle("show");
+}
+function myFunction7() {
+    var popup = document.getElementById("myPopup7");
+    popup.classList.toggle("show");
+}
+
+var image_tracker = 'h';
+function like() {
+    var image = document.getElementById('like');
+    if(image_tracker == 'h') {
+        image.src = 'icons/heart.png';
+        image_tracker = 'h2';
+    } else {
+        image.src = 'icons/heart.svg';
+        image_tracker = 'h';
+    }
+}
+function like2() {
+    var image = document.getElementById('like2');
+    if(image_tracker == 'h') {
+        image.src = 'icons/heart.png';
+        image_tracker = 'h2';
+    } else {
+        image.src = 'icons/heart.svg';
+        image_tracker = 'h';
+    }
+}
+function like3() {
+    var image = document.getElementById('like3');
+    if(image_tracker == 'h') {
+        image.src = 'icons/heart.png';
+        image_tracker = 'h2';
+    } else {
+        image.src = 'icons/heart.svg';
+        image_tracker = 'h';
+    }
+}
+function like4() {
+    var image = document.getElementById('like4');
+    if(image_tracker == 'h') {
+        image.src = 'icons/heart.png';
+        image_tracker = 'h2';
+    } else {
+        image.src = 'icons/heart.svg';
+        image_tracker = 'h';
+    }
+}
+function like5() {
+    var image = document.getElementById('like5');
+    if(image_tracker == 'h') {
+        image.src = 'icons/heart.png';
+        image_tracker = 'h2';
+    } else {
+        image.src = 'icons/heart.svg';
+        image_tracker = 'h';
+    }
+}
+function like6() {
+    var image = document.getElementById('like6');
+    if(image_tracker == 'h') {
+        image.src = 'icons/heart.png';
+        image_tracker = 'h2';
+    } else {
+        image.src = 'icons/heart.svg';
+        image_tracker = 'h';
+    }
+}
+function like7() {
+    var image = document.getElementById('like7');
+    if(image_tracker == 'h') {
+        image.src = 'icons/heart.png';
+        image_tracker = 'h2';
+    } else {
+        image.src = 'icons/heart.svg';
+        image_tracker = 'h';
+    }
+}
